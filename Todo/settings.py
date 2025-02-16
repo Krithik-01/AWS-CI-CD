285bf97bed56dfbba754887c55ad391bab99b90a
krithik k <97529471+Krithik-01@users.noreply.github.com>
2025-02-16 16:03:57 +0530

    Update settings.py
    fixes

--- Todo/settings.py
+++ Todo/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-86kx#ctdjf9%et6l9p8#+56lrkyyx9jo@jbhk*h6c()7@!c@#@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -117,6 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
